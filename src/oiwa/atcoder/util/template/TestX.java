0c52bef9e2baceaf9b0aad2cb165f741d8f1c7a3
Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-05-17 10:06:46 +0900

    テンプレ微修正

--- src/oiwa/atcoder/util/template/TestX.java
+++ src/oiwa/atcoder/util/template/TestX.java
@@ -7,9 +7,9 @@ public class TestX {
 	public static void main(String[] args) {
 		new Tester(Main.class, new Case[] {
 				
-				new Case(true,
-						"looooooooooooooing_loooooooooooooooooooooooooong_input",
-						"looooooooooooooing_loooooooooooooooooooooooooong_input\r\n"),
+				new Case(false,
+						"",
+						""),
 				new Case(false,
 						"",
 						""),
