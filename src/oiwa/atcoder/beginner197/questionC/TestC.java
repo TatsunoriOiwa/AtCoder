0b51e2f207ac38639c8902724cf0fb8bfc4a9d84
T.Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-05-18 08:36:26 +0900

    b197 preparation

---
+++ src/oiwa/atcoder/beginner197/questionC/TestC.java
@@ -0,0 +1,30 @@
+package oiwa.atcoder.beginner197.questionC;
+
+import oiwa.atcoder.util.Case;
+import oiwa.atcoder.util.Tester;
+
+public class TestC {
+	public static void main(String[] args) {
+		new Tester(Main.class, new Case[] {
+				
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+		});
+	}
+}
---
+++ src/oiwa/atcoder/beginner197/questionF/TestF.java
@@ -0,0 +1,30 @@
+package oiwa.atcoder.beginner197.questionF;
+
+import oiwa.atcoder.util.Case;
+import oiwa.atcoder.util.Tester;
+
+public class TestF {
+	public static void main(String[] args) {
+		new Tester(Main.class, new Case[] {
+				
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+		});
+	}
+}
