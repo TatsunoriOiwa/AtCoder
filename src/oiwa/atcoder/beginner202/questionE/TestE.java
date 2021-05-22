71881b0ff5310609f33660b800191f4c37a4cb57
Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-05-22 18:58:51 +0900

    abs202 preparation

---
+++ src/oiwa/atcoder/beginner202/questionE/TestE.java
@@ -0,0 +1,30 @@
+package oiwa.atcoder.beginner202.questionE;
+
+import oiwa.atcoder.util.Case;
+import oiwa.atcoder.util.Tester;
+
+public class TestE {
+	public static void main(String[] args) {
+		new Tester(Main.class, new Case[] {
+				
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+		});
+	}
+}
