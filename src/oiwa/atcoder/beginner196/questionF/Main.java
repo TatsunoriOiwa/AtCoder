7dd0457c7092f0bd87c105c87234501fd0a694e3
T.Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-05-26 08:57:14 +0900

    abc196 preparation

---
+++ src/oiwa/atcoder/beginner196/questionF/Main.java
@@ -0,0 +1,184 @@
+package oiwa.atcoder.beginner196.questionF;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.NoSuchElementException;
+
+public class Main {
+	public static void main(String[] args) {
+		PrintWriter out = new PrintWriter(System.out);
+		new Main().run(out);
+		out.flush();
+	}
+	
+	
+	public void run(PrintWriter out) {
+		@SuppressWarnings("unused")
+		FastScanner sc = new FastScanner();
+//		int i = sc.nextInt();
+//		String s = sc.next();
+//		out.println(sc.next());
+	}
+	
+	
+	
+	// ==== Fast Util ====
+	
+	static class FastScanner {
+		private final InputStream in = System.in;
+		private final byte[] buffer = new byte[1024];
+		private int ptr = 0;
+		private int buflen = 0;
+		private boolean hasNextByte() {
+			if (ptr < buflen) { return true; }
+			else {
+				ptr = 0;
+				try { buflen = in.read(buffer); }
+				catch (IOException e) { e.printStackTrace(); }
+				if (buflen <= 0) { return false; }
+			}
+			return true;
+		}
+		private int readByte() {
+			if (hasNextByte()) return buffer[ptr++];
+			else return -1;
+		}
+		private static boolean isPrintableChar(int c) {
+			return 33 <= c && c <= 126;
+		}
+		public boolean hasNext() {
+			while (hasNextByte() && !isPrintableChar(buffer[ptr])) ptr++;
+			return hasNextByte();
+		}
+		public String next() {
+			if (!hasNext()) throw new NoSuchElementException();
+			StringBuilder sb = new StringBuilder();
+			int b = readByte();
+			while (isPrintableChar(b)) {
+				sb.appendCodePoint(b);
+				b = readByte();
+			}
+			return sb.toString();
+		}
+		public long nextLong() {
+			if (!hasNext()) throw new NoSuchElementException();
+			long n = 0;
+			boolean minus = false;
+			int b = readByte();
+			if (b == '-') {
+				minus = true;
+				b = readByte();
+			}
+			if (b < '0' || '9' < b) { throw new NumberFormatException(); }
+			while (true) {
+				if ('0' <= b && b <= '9') {
+					n *= 10;
+					n += b - '0';
+				} else if (b == -1 || !isPrintableChar(b)) {
+					return minus ? -n : n;
+				} else {
+					throw new NumberFormatException();
+				}
+				b = readByte();
+			}
+		}
+		public int nextInt() {
+			long nl = nextLong();
+			if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE) throw new NumberFormatException();
+			return (int) nl;
+		}
+		public double nextDouble() { return Double.parseDouble(next()); }
+		public int[] nextIntArray(final int N) {
+			int[] ret = new int[N];
+			for (int i = 0; i < N; i++) { ret[i] = this.nextInt(); }
+			return ret;
+		}
+		public long[] nextLongArray(final int N) {
+			long[] ret = new long[N];
+			for (int i = 0; i < N; i++) { ret[i] = this.nextLong(); }
+			return ret;
+		}
+		/**
+		 *  each element will be A_i + offset<br>
+		 * O(N)
+		 */
+		public int[] nextIntArray(final int N, int offset) {
+			int[] ret = new int[N];
+			for (int i = 0; i < N; i++) { ret[i] = this.nextInt() + offset; }
+			return ret;
+		}
+		/**
+		 *  each element will be A_i + offset<br>
+		 *  O(N)
+		 */
+		public long[] nextLongArray(final int N, long offset) {
+			long[] ret = new long[N];
+			for (int i = 0; i < N; i++) { ret[i] = this.nextLong() + offset; }
+			return ret;
+		}
+	}
+	
+	public static class AtMath {
+		/**
+		 * Returns 0 if n < r.<br>
+		 * When n > 20 product will be grater than long_max.<br>
+		 * O(n) or O(2min(r, n-r)) more precisely.
+		 * @param n
+		 * @param r
+		 * @return nCr
+		 */
+		public static long nCr(int n, int r) {
+			if (n < r) return 0;
+			r = Math.min(r, n-r);
+			if (r == 0) return 1;
+			BigInteger res = BigInteger.valueOf(1L);
+			final int d = n-r;
+			for (int i = n; i > d; i--) { res = res.multiply(BigInteger.valueOf(i)); }
+			for (int i = 2; i <= r; i++) { res = res.divide(BigInteger.valueOf(i)); }
+			return res.longValue();
+		}
+		/**
+		 * Use {@link Math#pow Math.pow} for double.
+		 * @param x
+		 * @param n
+		 * @return
+		 */
+		public static long power(long x, int n) {
+			assert(n >= 0);
+			if (n == 0) return 1;
+			if (n == 1) return x;
+			long res = 1;
+			for (int i = 31; i >= 0; i--) { // 31: number of bits of integer.
+				res *= res;
+				if ((n & (1<<i)) != 0) {res *= x;}
+			}
+			return res;
+		}
+		
+		/**
+		 * O(1) or strictly O(31).
+		 * @param x
+		 * @param n
+		 * @param mod
+		 * @return
+		 */
+		public static long power(long x, int n, long mod) {
+			assert(n >= 0);
+			if (n == 0) return 1;
+			if (n == 1) return x;
+			x %= mod;
+			long res = 1;
+			for (int i = 31; i >= 0; i--) { // 31: number of bits of integer.
+				res *= res;
+				res %= mod;
+				if ((n & (1<<i)) != 0) {
+					res *= x;
+					res %= mod;
+				}
+			}
+			return res;
+		}
+	}
+}
