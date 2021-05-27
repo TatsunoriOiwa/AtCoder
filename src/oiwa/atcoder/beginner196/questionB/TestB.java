42b5aa9de9baabec647d570415db9ea1dcbd0855
T.Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-05-27 14:41:33 +0900

    abc196A 3 min, abc196B 15 min

--- src/oiwa/atcoder/beginner196/questionB/TestB.java
+++ src/oiwa/atcoder/beginner196/questionB/TestB.java
@@ -7,15 +7,21 @@ public class TestB {
 	public static void main(String[] args) {
 		new TesterV3(Main.class, new Case[] {
 				
-				new Case(false,
-						"",
-						""),
-				new Case(false,
-						"",
-						""),
-				new Case(false,
-						"",
-						""),
+				new Case(true,
+						"5.90\r\n"
+						+ "",
+						"5\r\n"
+						+ ""),
+				new Case(true,
+						"0\r\n"
+						+ "",
+						"0\r\n"
+						+ ""),
+				new Case(true,
+						"84939825309432908832902189.9092309409809091329\r\n"
+						+ "",
+						"84939825309432908832902189\r\n"
+						+ ""),
 				new Case(false,
 						"",
 						""),
