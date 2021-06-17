9fee65da69e5326da0cb0489712ab9a7b0910e50
T.Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-06-17 15:42:43 +0900

    テンプレート更新　debugに配列を追加

--- src/oiwa/atcoder/util/template/Main.java
+++ src/oiwa/atcoder/util/template/Main.java
@@ -201,4 +201,25 @@ public class Main {
 	public void debug(String str) {
 		if (DEBUG) System.out.println(str);
 	}
+	public void debug(boolean[] arr) {
+		if (DEBUG) {
+			System.out.print("[");
+			for (int i = 0; i < arr.length; i++) { if (i != 0) System.out.println(","); System.out.print(arr[i]); }
+			System.out.println("]");
+		}
+	}
+	public void debug(long[] arr) {
+		if (DEBUG) {
+			System.out.print("[");
+			for (int i = 0; i < arr.length; i++) { if (i != 0) System.out.println(","); System.out.print(arr[i]); }
+			System.out.println("]");
+		}
+	}
+	public void debug(double[] arr) {
+		if (DEBUG) {
+			System.out.print("[");
+			for (int i = 0; i < arr.length; i++) { if (i != 0) System.out.println(","); System.out.print(arr[i]); }
+			System.out.println("]");
+		}
+	}
 }
