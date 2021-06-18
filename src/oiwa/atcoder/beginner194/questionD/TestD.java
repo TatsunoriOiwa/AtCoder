045d6c4ae406eef2e8a410ede5c026a6fae1410e
T.Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-06-18 17:22:59 +0900

    abc194 prepare, abc 194 D 5 min. 答えを見た

---
+++ src/oiwa/atcoder/beginner194/questionD/TestD.java
@@ -0,0 +1,34 @@
+package oiwa.atcoder.beginner194.questionD;
+
+import oiwa.atcoder.util.Case;
+import oiwa.atcoder.util.TesterV3;
+
+public class TestD {
+	public static void main(String[] args) {
+		new TesterV3(Main.class, new Case[] {
+				
+				new Case(true,
+						"2\r\n"
+						+ "",
+						"2.00000000000\r\n"
+						+ ""),
+				new Case(true,
+						"3\r\n"
+						+ "",
+						"4.50000000000\r\n"
+						+ ""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+		});
+	}
+}
---
+++ src/oiwa/atcoder/beginner194/questionF/TestF.java
@@ -0,0 +1,30 @@
+package oiwa.atcoder.beginner194.questionF;
+
+import oiwa.atcoder.util.Case;
+import oiwa.atcoder.util.TesterV3;
+
+public class TestF {
+	public static void main(String[] args) {
+		new TesterV3(Main.class, new Case[] {
+				
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+		});
+	}
+}
