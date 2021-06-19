549874f7464c276d9010a2a7dab2b2d2b898d424
Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-06-19 19:44:24 +0900

    abc 206 preparation

---
+++ src/oiwa/atcoder/beginner206/questionB/TestB.java
@@ -0,0 +1,30 @@
+package oiwa.atcoder.beginner206.questionB;
+
+import oiwa.atcoder.util.Case;
+import oiwa.atcoder.util.TesterV3;
+
+public class TestB {
+	public static void main(String[] args) {
+		new TesterV3(Main.class, new Case[] {
+				
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+		});
+	}
+}
