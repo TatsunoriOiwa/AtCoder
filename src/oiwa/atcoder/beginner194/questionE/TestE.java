8dabbaa5fae80f172d8a8ac44175cf0d8beb0d08
Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-06-23 21:11:35 +0900

    ABC 194 E 30 min.

--- src/oiwa/atcoder/beginner194/questionE/TestE.java
+++ src/oiwa/atcoder/beginner194/questionE/TestE.java
@@ -7,18 +7,30 @@ public class TestE {
 	public static void main(String[] args) {
 		new TesterV3(Main.class, new Case[] {
 				
-				new Case(false,
-						"",
-						""),
-				new Case(false,
-						"",
-						""),
-				new Case(false,
-						"",
-						""),
-				new Case(false,
-						"",
-						""),
+				new Case(true,
+						"3 2\r\n"
+						+ "0 0 1\r\n"
+						+ "",
+						"1\r\n"
+						+ ""),
+				new Case(true,
+						"3 2\r\n"
+						+ "1 1 1\r\n"
+						+ "",
+						"0\r\n"
+						+ ""),
+				new Case(true,
+						"3 2\r\n"
+						+ "0 1 0\r\n"
+						+ "",
+						"2\r\n"
+						+ ""),
+				new Case(true,
+						"7 3\r\n"
+						+ "0 0 1 2 0 1 0\r\n"
+						+ "",
+						"2\r\n"
+						+ ""),
 				new Case(false,
 						"",
 						""),
