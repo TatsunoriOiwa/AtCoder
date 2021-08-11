22f4d1294e8eb608cd21559e036677c050018480
Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-08-11 21:08:09 +0900

    ABC192D failed 途中

--- src/oiwa/atcoder/beginner192/questionD/Main.java
+++ src/oiwa/atcoder/beginner192/questionD/Main.java
@@ -17,11 +17,51 @@ public class Main {
 	
 	
 	public void run(PrintWriter out) {
-		@SuppressWarnings("unused")
 		FastScanner sc = new FastScanner();
 //		int i = sc.nextInt();
 //		String s = sc.next();
 //		out.println(sc.next());
+		
+		String X = sc.next();
+		long M = sc.nextLong();
+		
+		BigInteger[] xs = new BigInteger[X.length()];
+		int max = 1;
+		for (int i = 0; i < X.length(); i++) {
+			int c = X.charAt(i) - '0';
+			xs[i] = BigInteger.valueOf(c);
+			if (max < c) max = c;
+		}
+		int cn = X.charAt(0) - '0';
+//		debug("cn=" + cn + " max=" + max);
+		long d = xs.length == 1 ? 2 : Math.round(Math.floor(Math.pow(M / (cn + 1.0d), 1.0d / (xs.length - 1))));
+//		debug(M / (cn + 1.0d));
+//		debug("d= " + d);
+		if (d <= max) d = max + 1;
+		long cnt = d - max - 1;
+		for (;; d++) {
+//			debug(d);
+//			long xd = 0;
+			BigInteger xd = BigInteger.ZERO;
+			BigInteger dd = BigInteger.valueOf(d);
+			for (BigInteger ci : xs) {
+				xd = xd.multiply(dd).add(ci);
+//				if (xd.bitLength() > 63) break;
+			}
+//			debug(xd.longValue());
+			try {
+				if (xd.bitLength() < 64 && xd.longValueExact() <= M) {
+					cnt++;
+//					debug(xd.longValueExact());
+				} else {
+					break;
+				}
+			} catch (Exception e) {
+				break;
+			}
+		}
+		
+		out.println(cnt);
 	}
 	
 	
@@ -210,28 +250,36 @@ public class Main {
 			return res;
 		}
 	}
-	
+	public void debug(long v) { if (DEBUG) System.out.println(v); }
+	public void debug(double v) { if (DEBUG) System.out.println(v); }
 	public void debug(String str) {
 		if (DEBUG) System.out.println(str);
 	}
 	public void debug(boolean[] arr) {
 		if (DEBUG) {
 			System.out.print("[");
-			for (int i = 0; i < arr.length; i++) { if (i != 0) System.out.println(","); System.out.print(arr[i]); }
+			for (int i = 0; i < arr.length; i++) { if (i != 0) System.out.print(","); System.out.print(arr[i]); }
+			System.out.println("]");
+		}
+	}
+	public void debug(int[] arr) {
+		if (DEBUG) {
+			System.out.print("[");
+			for (int i = 0; i < arr.length; i++) { if (i != 0) System.out.print(","); System.out.print(arr[i]); }
 			System.out.println("]");
 		}
 	}
 	public void debug(long[] arr) {
 		if (DEBUG) {
 			System.out.print("[");
-			for (int i = 0; i < arr.length; i++) { if (i != 0) System.out.println(","); System.out.print(arr[i]); }
+			for (int i = 0; i < arr.length; i++) { if (i != 0) System.out.print(","); System.out.print(arr[i]); }
 			System.out.println("]");
 		}
 	}
 	public void debug(double[] arr) {
 		if (DEBUG) {
 			System.out.print("[");
-			for (int i = 0; i < arr.length; i++) { if (i != 0) System.out.println(","); System.out.print(arr[i]); }
+			for (int i = 0; i < arr.length; i++) { if (i != 0) System.out.print(","); System.out.print(arr[i]); }
 			System.out.println("]");
 		}
 	}
