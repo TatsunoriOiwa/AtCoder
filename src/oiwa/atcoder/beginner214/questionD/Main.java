d6531d80d9fe3f320322859a630c99d10dfd3880
Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-08-14 17:13:44 +0900

    ABC214 prep2

--- src/oiwa/atcoder/beginner214/questionD/Main.java
+++ src/oiwa/atcoder/beginner214/questionD/Main.java
@@ -17,7 +17,6 @@ public class Main {
 	
 	
 	public void run(PrintWriter out) {
-		@SuppressWarnings("unused")
 		FastScanner sc = new FastScanner();
 //		int i = sc.nextInt();
 //		String s = sc.next();
