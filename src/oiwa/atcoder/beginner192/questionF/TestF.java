c7def8b286fba7045330a2d83d02d2a5a901bc84
Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-08-15 20:08:10 +0900

    ABC192E failed

--- src/oiwa/atcoder/beginner192/questionF/TestF.java
+++ src/oiwa/atcoder/beginner192/questionF/TestF.java
@@ -7,12 +7,18 @@ public class TestF {
 	public static void main(String[] args) {
 		new TesterV10(Main.class, new Case[] {
 				
-				new Case(false,
-						"",
-						""),
-				new Case(false,
-						"",
-						""),
+				new Case(true,
+						"3 9999999999\r\n"
+						+ "3 6 8\r\n"
+						+ "",
+						"4999999994\r\n"
+						+ ""),
+				new Case(true,
+						"1 1000000000000000000\r\n"
+						+ "1\r\n"
+						+ "",
+						"999999999999999999\r\n"
+						+ ""),
 				new Case(false,
 						"",
 						""),
