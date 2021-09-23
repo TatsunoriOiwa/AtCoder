5c7896d02d24b191846365354f46cb44bfa50a55
T.Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-09-23 17:29:30 +0900

    ABC 192 D 3rd, 18 min.

---
+++ src/oiwa/atcoder/beginner192/questionD3/TestD3.java
@@ -0,0 +1,39 @@
+package oiwa.atcoder.beginner192.questionD3;
+
+import oiwa.atcoder.util.Case;
+import oiwa.atcoder.util.TesterV10;
+
+public class TestD3 {
+	public static void main(String[] args) {
+		new TesterV10(Main.class, new Case[] {
+				
+				new Case(true,
+						"22\r\n"
+						+ "10\r\n"
+						+ "",
+						"2\r\n"
+						+ ""),
+				new Case(true,
+						"999\r\n"
+						+ "1500\r\n"
+						+ "",
+						"3\r\n"
+						+ ""),
+				new Case(true,
+						"100000000000000000000000000000000000000000000000000000000000\r\n"
+						+ "1000000000000000000\r\n"
+						+ "",
+						"1\r\n"
+						+ ""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+		});
+	}
+}
