01bb54905bbaecfbabd7e1253e3eb4b5bb0e5ca1
T.Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-10-29 17:28:57 +0900

    ABC 186 preparation

---
+++ src/main/oiwa/atcoder/beginner186/questionC/TestC.java
@@ -0,0 +1,36 @@
+package oiwa.atcoder.beginner186.questionC;
+
+import oiwa.atcoder.util.Case;
+import oiwa.atcoder.util.TesterV10;
+
+public class TestC {
+	public static void main(String[] args) {
+		new TesterV10(Main.class, new Case[] {
+				
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+		});
+	}
+}
