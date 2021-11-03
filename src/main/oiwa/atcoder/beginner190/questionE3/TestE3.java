8f968b82418f0fb41128d332860c3b53011a4759
T.Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-11-03 16:50:57 +0900

    ABC 190 E 3rd

---
+++ src/main/oiwa/atcoder/beginner190/questionE3/TestE3.java
@@ -0,0 +1,64 @@
+package oiwa.atcoder.beginner190.questionE3;
+
+import oiwa.atcoder.util.Case;
+import oiwa.atcoder.util.TesterV10;
+
+public class TestE3 {
+	public static void main(String[] args) {
+		new TesterV10(Main.class, new Case[] {
+				
+				new Case(true,
+						"4 3\r\n"
+						+ "1 4\r\n"
+						+ "2 4\r\n"
+						+ "3 4\r\n"
+						+ "3\r\n"
+						+ "1 2 3\r\n"
+						+ "",
+						"5\r\n"
+						+ ""),
+				new Case(true,
+						"4 3\r\n"
+						+ "1 4\r\n"
+						+ "2 4\r\n"
+						+ "1 2\r\n"
+						+ "3\r\n"
+						+ "1 2 3\r\n"
+						+ "",
+						"-1\r\n"
+						+ ""),
+				new Case(true,
+						"10 10\r\n"
+						+ "3 9\r\n"
+						+ "3 8\r\n"
+						+ "8 10\r\n"
+						+ "2 10\r\n"
+						+ "5 8\r\n"
+						+ "6 8\r\n"
+						+ "5 7\r\n"
+						+ "6 7\r\n"
+						+ "1 6\r\n"
+						+ "2 4\r\n"
+						+ "4\r\n"
+						+ "1 2 7 9\r\n"
+						+ "",
+						"11\r\n"
+						+ ""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+		});
+	}
+}
