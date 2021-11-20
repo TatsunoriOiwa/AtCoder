b1eb9efa577eadaae0e01cbd4cb695738e38cd0b
T.Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-11-20 20:20:54 +0900

    ABC 188 F 3rd

---
+++ src/main/oiwa/atcoder/beginner188/questionF3/TestF3.java
@@ -0,0 +1,42 @@
+package oiwa.atcoder.beginner188.questionF3;
+
+import oiwa.atcoder.util.Case;
+import oiwa.atcoder.util.TesterV10;
+
+public class TestF3 {
+	public static void main(String[] args) {
+		new TesterV10(Main.class, new Case[] {
+				
+				new Case(true,
+						"3 9\r\n"
+						+ "",
+						"3\r\n"
+						+ ""),
+				new Case(true,
+						"7 11\r\n"
+						+ "",
+						"3\r\n"
+						+ ""),
+				new Case(true,
+						"1000000000000000000 1000000000000000000\r\n"
+						+ "",
+						"0\r\n"
+						+ ""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+		});
+	}
+}
