6cde11eb077013028bc681500702798ac8d255b9
T.Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-11-25 17:07:02 +0900

    ABC 200 E 2nd

---
+++ src/main/oiwa/atcoder/beginner200/questionE2/TestE2.java
@@ -0,0 +1,68 @@
+package oiwa.atcoder.beginner200.questionE2;
+
+import oiwa.atcoder.util.Case;
+import oiwa.atcoder.util.TesterV10;
+
+public class TestE2 {
+	public static void main(String[] args) {
+		new TesterV10(Main.class, new Case[] {
+				
+				new Case(true,
+						"2 5\r\n"
+						+ "",
+						"1 2 2\r\n"
+						+ ""),
+				new Case(true,
+						"2 1\r\n"
+						+ "",
+						"1 1 1\r\n"
+						+ ""),
+				new Case(true,
+						"2 2\r\n"
+						+ "",
+						"1 1 2\r\n"
+						+ ""),
+				new Case(true,
+						"2 3\r\n"
+						+ "",
+						"1 2 1\r\n"
+						+ ""),
+				new Case(true,
+						"2 4\r\n"
+						+ "",
+						"2 1 1\r\n"
+						+ ""),
+				new Case(true,
+						"2 8\r\n"
+						+ "",
+						"2 2 2\r\n"
+						+ ""),
+
+				new Case(true,
+						"1000000 1000000000000000000\r\n"
+						+ "",
+						"1000000 1000000 1000000\r\n"
+						+ ""),
+				new Case(true,
+						"9 47\r\n"
+						+ "",
+						"3 1 4\r\n"
+						+ ""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+		});
+	}
+}
