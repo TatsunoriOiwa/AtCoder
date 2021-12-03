0ca202b093076aca06e826c46e83358861d83e70
Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-12-03 20:54:07 +0900

    ABC 230 preparation

---
+++ src/main/oiwa/atcoder/beginner230/questionA/TestA.java
@@ -0,0 +1,36 @@
+package oiwa.atcoder.beginner230.questionA;
+
+import oiwa.atcoder.util.Case;
+import oiwa.atcoder.util.TesterV10;
+
+public class TestA {
+	public static void main(String[] args) {
+		new TesterV10(Main.class, new Case[] {
+				
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+		});
+	}
+}
