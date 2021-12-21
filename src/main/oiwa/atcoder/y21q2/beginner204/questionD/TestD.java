b54e8e3cafd2ad2291e45b6a04e64fe5ff81d49e
T.Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-12-21 13:22:45 +0900

    ABC 204 preparation

---
+++ src/main/oiwa/atcoder/y21q2/beginner204/questionD/TestD.java
@@ -0,0 +1,36 @@
+package oiwa.atcoder.y21q2.beginner204.questionD;
+
+import oiwa.atcoder.util.Case;
+import oiwa.atcoder.util.TesterV10;
+
+public class TestD {
+	public static void main(String[] args) {
+		new TesterV10(Main.class, new Case[] {
+				
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+				new Case(false,
+						"",
+						""),
+		});
+	}
+}
